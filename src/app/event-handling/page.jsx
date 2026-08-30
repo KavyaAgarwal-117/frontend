858be702979833593ae5a6f341a122e4cf8e3b2a
KavyaAgarwal-117 <kavyaagarwal1708@gmail.com>
2026-08-30 19:10:00 +0530

    Event Handling

--- src/app/event-handling/page.jsx
+++ src/app/event-handling/page.jsx
@@ -1,44 +1,59 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
-const EventHandling  = () => {
+const EventHandling = () => {
   return (
-    <div className='min-h-screen'>
+    <div className="min-h-screen">
+      <div className="container mx-auto my-10 ">
+        <h1 className="text-3xl font-bold text-center mb-5">Event Handling</h1>
 
-        <div className='container mx-auto my-10 '>
-            <h1 className='text-3xl font-bold text-center mb-5'>Event Handling</h1>
+        <button
+          className="p-2 border"
+          onClick={() => {
+            alert("button clicked");
+          }}
+        >
+          Click Me
+        </button>
 
-            <button className='p-2 border'
-            onClick={() => { alert('button clicked') }}>Click Me</button>
+        <input
+          type="text"
+          className="border p-2 block mt-4"
+          // onChange={(e) => { console.log(e.target.value); }}/>
+          //  the e in paranthesis will help to print the current input value so if we delete/change the value it will show print it on the console and also show the changes */}
 
-            <input type="text" className='border p-2 block mt-4'
-
-            // onChange={(e) => { console.log(e.target.value); }}/>
-            //  the e in paranthesis will help to print the current input value so if we delete/change the value it will show print it on the console and also show the changes */}
-
-
-            onChange={(e) => { console.log(e.target.value); }}/>
-
-
-            <input type="text" className='border p-2 block mt-4'
-            onKeyDown={(e) => {console.log(e.code);
-            }}/>
-
-            <input type="color" 
-            onChange={(e) => { document.body.style.backgroundColor = e.target.value;
+          onChange={(e) => {
             console.log(e.target.value);
-            }}/>
+          }}
+        />
 
-            {/* The upper line document one will show the color changes in the whole output screen without this it won't show on the output screen but the value will be shown on console  */}
+        <input
+          type="text"
+          className="border p-2 block mt-4"
+          onKeyDown={(e) => {
+            console.log(e.code);
+          }}
+        />
 
-            <input type="file" 
-            onChange={(e) => { console.log(e.target.files);
-            }}/>
+        <input
+          type="color"
+          onChange={(e) => {
+            document.body.style.backgroundColor = e.target.value;
+            console.log(e.target.value);
+          }}
+        />
 
-        </div>
+        {/* The upper line document one will show the color changes in the whole output screen without this it won't show on the output screen but the value will be shown on console  */}
 
+        <input
+          type="file"
+          onChange={(e) => {
+            console.log(e.target.files);
+          }}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default EventHandling;
