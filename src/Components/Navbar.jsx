a4cce6f4d873e4916dd6207a54f3f9f0e99d126a
KavyaAgarwal-117 <kavyaagarwal1708@gmail.com>
2026-08-30 19:07:27 +0530

    Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -21,12 +21,19 @@ const Navbar = () => {
           <span className="ml-3 text-xl">Tailblocks</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <Link className="mr-5 hover:text-gray-900" href="/">Home</Link>
-          <Link className="mr-5 hover:text-gray-900" href="/login">Login</Link>
-          <Link className="mr-5 hover:text-gray-900" href="/signup">Signup</Link>
-          <Link className="mr-5 hover:text-gray-900" href="/event-handling">Event-Handling</Link>
+          <Link className="mr-5 hover:text-gray-900" href="/">
+            Home
+          </Link>
+          <Link className="mr-5 hover:text-gray-900" href="/login">
+            Login
+          </Link>
+          <Link className="mr-5 hover:text-gray-900" href="/signup">
+            Signup
+          </Link>
+          <Link className="mr-5 hover:text-gray-900" href="/event-handling">
+            Event-Handling
+          </Link>
           {/*link tag only be used with href tag*/}
-          
         </nav>
         <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
           Button
